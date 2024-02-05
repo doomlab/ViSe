3ccda8912d4366bcc3ccb11994a7b864c7aba570
The Doom Lab <buchananlab@gmail.com>
2024-02-04 20:09:11 -0500

    updating the visualization functions

--- R/visualize_c_map.R
+++ R/visualize_c_map.R
@@ -6,12 +6,36 @@
 #' effect size and correlation to see how they map onto
 #' the proposed c value.
 #'
-#' @param dlow The lower limit of the possible effect size
-#' @param dvalues A vector of effect size values that are
+#'
+#' @param dlow The lower limit of the possible effect size (required).
+#' @param r_values A vector of correlation values that are
+#' possible (required).
+#' @param lower Use this to indicate if you want the lower or upper bound
+#' of d for one sided confidence intervals. If d is positive, you generally
+#' want \code{lower = TRUE}, while negative d values should enter
+#' \code{lower = FALSE} for the upper bound that is closer to zero (required).
+#' @param d_values A vector of effect size values that are
 #' possible.
-#' @param rvalues A vector of correlation values that are
+#' @param f_values A vector of f effect size values that are
 #' possible.
-#' @return Returns a pretty graph
+#' @param f2_values A vector of f2 effect size values that are
+#' possible.
+#' @param nnt_values A vector of number needed to treat
+#' effect size values that are possible.
+#' @param prob_values A vector of probability of superiority
+#' effect size values that are possible.
+#' @param prop_u1_values A vector of proportion of overlap u1
+#' effect size values that are possible.
+#' @param prop_u2_values A vector of proportion of overlap u2
+#' effect size values that are possible.
+#' @param prop_u3_values A vector of proportion of overlap u3
+#' effect size values that are possible.
+#' @param prop_overlap_values A vector of proportion of
+#' distribution overlap effect size values that are possible.
+#'
+#' @return Returns a pretty graph of the possible effect size
+#' and correlation combinations with the region of effect colored in.
+#' Note that all effect sizes are converted to d for the graph.
 #'
 #' \item{graph}{The graph of possible values for c}
 #'
@@ -21,8 +45,8 @@
 #' @examples
 #'
 #' visualize_c_map(dlow = .25,
-#'   dvalues = c(.2, .3, .8),
-#'   rvalues = c(.1, .4, .3),
+#'   d_values = c(.2, .3, .8),
+#'   r_values = c(.1, .4, .3),
 #'   lower = TRUE)
 #'
 #' @rdname visualize_c_map
@@ -36,10 +60,10 @@ visualize_c_map <- function (dlow,
                              f2_values = NULL,
                              nnt_values = NULL,
                              prob_values = NULL,
-                             propu1_values = NULL,
-                             propu2_values = NULL,
-                             propu3_values = NULL,
-                             propover_values = NULL,
+                             prop_u1_values = NULL,
+                             prop_u2_values = NULL,
+                             prop_u3_values = NULL,
+                             prop_overlap_values = NULL,
                              lower = TRUE) {
 
   if(missing(dlow) | missing(r_values)){
@@ -49,6 +73,7 @@ visualize_c_map <- function (dlow,
   # make the plot
   graph <- visualize_c(dlow = dlow, lower = lower)$graph
 
+  d <- r <- NULL
 
   if(!is.null(d_values)){
     # make the combination of d and r values
@@ -79,6 +104,154 @@ visualize_c_map <- function (dlow,
         DF_points2 <- DF_points2_temp
       }
   }
+
+  if(!is.null(f2_values)){
+    # make the combination of d and r values
+    d_values <- 1:length(f2_values)
+    for (i in 1:length(f2_values)){
+      d_values[i] <- other_to_d(f2 = f2_values[i])
+    }
+
+    DF_points_temp <- expand.grid(d_values, r_values)
+    DF_points2_temp <- expand.grid(d_values, r_values)
+    colnames(DF_points_temp) <- colnames(DF_points2_temp) <- c("d", "r")
+    DF_points_temp$label <- DF_points2_temp$label <- paste0("f2 = ", f2_values)
+
+    if (exists("DF_points")){
+      DF_points <- rbind(DF_points, DF_points_temp)
+      DF_points2 <- rbind(DF_points2, DF_points2_temp)
+    } else {
+      DF_points <- DF_points_temp
+      DF_points2 <- DF_points2_temp
+    }
+  }
+
+  if(!is.null(nnt_values)){
+    # make the combination of d and r values
+    d_values <- 1:length(nnt_values)
+    for (i in 1:length(nnt_values)){
+      d_values[i] <- other_to_d(nnt = nnt_values[i])
+    }
+
+    DF_points_temp <- expand.grid(d_values, r_values)
+    DF_points2_temp <- expand.grid(d_values, r_values)
+    colnames(DF_points_temp) <- colnames(DF_points2_temp) <- c("d", "r")
+    DF_points_temp$label <- DF_points2_temp$label <- paste0("nnt = ", nnt_values)
+
+    if (exists("DF_points")){
+      DF_points <- rbind(DF_points, DF_points_temp)
+      DF_points2 <- rbind(DF_points2, DF_points2_temp)
+    } else {
+      DF_points <- DF_points_temp
+      DF_points2 <- DF_points2_temp
+    }
+  }
+
+  if(!is.null(prob_values)){
+    # make the combination of d and r values
+    d_values <- 1:length(prob_values)
+    for (i in 1:length(prob_values)){
+      d_values[i] <- other_to_d(prob = prob_values[i])
+    }
+
+    DF_points_temp <- expand.grid(d_values, r_values)
+    DF_points2_temp <- expand.grid(d_values, r_values)
+    colnames(DF_points_temp) <- colnames(DF_points2_temp) <- c("d", "r")
+    DF_points_temp$label <- DF_points2_temp$label <- paste0("prob = ", prob_values)
+
+    if (exists("DF_points")){
+      DF_points <- rbind(DF_points, DF_points_temp)
+      DF_points2 <- rbind(DF_points2, DF_points2_temp)
+    } else {
+      DF_points <- DF_points_temp
+      DF_points2 <- DF_points2_temp
+    }
+  }
+
+  if(!is.null(prop_u1_values)){
+    # make the combination of d and r values
+    d_values <- 1:length(prop_u1_values)
+    for (i in 1:length(prop_u1_values)){
+      d_values[i] <- other_to_d(prop_u1 = prop_u1_values[i])
+    }
+
+    DF_points_temp <- expand.grid(d_values, r_values)
+    DF_points2_temp <- expand.grid(d_values, r_values)
+    colnames(DF_points_temp) <- colnames(DF_points2_temp) <- c("d", "r")
+    DF_points_temp$label <- DF_points2_temp$label <- paste0("prop_u1 = ", prop_u1_values)
+
+    if (exists("DF_points")){
+      DF_points <- rbind(DF_points, DF_points_temp)
+      DF_points2 <- rbind(DF_points2, DF_points2_temp)
+    } else {
+      DF_points <- DF_points_temp
+      DF_points2 <- DF_points2_temp
+    }
+  }
+
+  if(!is.null(prop_u2_values)){
+    # make the combination of d and r values
+    d_values <- 1:length(prop_u2_values)
+    for (i in 1:length(prop_u2_values)){
+      d_values[i] <- other_to_d(prop_u2 = prop_u2_values[i])
+    }
+
+    DF_points_temp <- expand.grid(d_values, r_values)
+    DF_points2_temp <- expand.grid(d_values, r_values)
+    colnames(DF_points_temp) <- colnames(DF_points2_temp) <- c("d", "r")
+    DF_points_temp$label <- DF_points2_temp$label <- paste0("prop_u2 = ", prop_u2_values)
+
+    if (exists("DF_points")){
+      DF_points <- rbind(DF_points, DF_points_temp)
+      DF_points2 <- rbind(DF_points2, DF_points2_temp)
+    } else {
+      DF_points <- DF_points_temp
+      DF_points2 <- DF_points2_temp
+    }
+  }
+
+  if(!is.null(prop_u3_values)){
+    # make the combination of d and r values
+    d_values <- 1:length(prop_u3_values)
+    for (i in 1:length(prop_u3_values)){
+      d_values[i] <- other_to_d(prop_u3 = prop_u3_values[i])
+    }
+
+    DF_points_temp <- expand.grid(d_values, r_values)
+    DF_points2_temp <- expand.grid(d_values, r_values)
+    colnames(DF_points_temp) <- colnames(DF_points2_temp) <- c("d", "r")
+    DF_points_temp$label <- DF_points2_temp$label <- paste0("prop_u3 = ", prop_u3_values)
+
+    if (exists("DF_points")){
+      DF_points <- rbind(DF_points, DF_points_temp)
+      DF_points2 <- rbind(DF_points2, DF_points2_temp)
+    } else {
+      DF_points <- DF_points_temp
+      DF_points2 <- DF_points2_temp
+    }
+  }
+
+  if(!is.null(prop_overlap_values)){
+    # make the combination of d and r values
+    d_values <- 1:length(prop_overlap_values)
+    for (i in 1:length(prop_overlap_values)){
+      d_values[i] <- other_to_d(prop_overlap = prop_overlap_values[i])
+    }
+
+    DF_points_temp <- expand.grid(d_values, r_values)
+    DF_points2_temp <- expand.grid(d_values, r_values)
+    colnames(DF_points_temp) <- colnames(DF_points2_temp) <- c("d", "r")
+    DF_points_temp$label <- DF_points2_temp$label <- paste0("prop_overlap = ", prop_overlap_values)
+
+    if (exists("DF_points")){
+      DF_points <- rbind(DF_points, DF_points_temp)
+      DF_points2 <- rbind(DF_points2, DF_points2_temp)
+    } else {
+      DF_points <- DF_points_temp
+      DF_points2 <- DF_points2_temp
+    }
+  }
+
 
   graph2 <- graph +
     geom_point(data = DF_points, aes(d, r, color = DF_points$label), size = 2) +
